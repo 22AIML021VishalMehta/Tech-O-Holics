af07447bb32901352a283f2053007fe836f4bd2c
Harsh772005 <22aiml003@charusat.edu.in>
2023-04-02 13:03:38 +0530

    second commit

---
+++ Harsh.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"My name is Bhanushali Harsh Ashvinkumar"<<endl;
+    int a,b,c;
+    cout<<"Enter the a and b:";
+    cin>>a>>b;
+    c = (a + b)/2;
+    cout<<"Sum of two numbers :"<<c;
+    
+    return 0;
+}
--- you_can_write_code_here.cpp
+++ you_can_write_code_here.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
- 
 int main()
 {
     cout << "Tech O'Holics!" << endl;
     cout << "2nd semester Group Project for CE144" << endl;
     //22aiml021
+    cout<<"Vishal gandho che::";
     return 0;
 }
