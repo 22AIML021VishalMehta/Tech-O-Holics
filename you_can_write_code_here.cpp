675f0228754aff62ffce27e1549b1427e6d7d5f1
22AIML021VishalMehta <22AIML021@charusat.edu.in>
2023-04-02 13:00:17 +0530

    first commit

--- you_can_write_code_here.cpp
+++ you_can_write_code_here.cpp
@@ -5,6 +5,7 @@ int main()
 {
     cout << "Tech O'Holics!" << endl;
     cout << "2nd semester Group Project for CE144" << endl;
+    cout << "Changes are made like this" << endl;
     //22aiml021
     return 0;
 }
