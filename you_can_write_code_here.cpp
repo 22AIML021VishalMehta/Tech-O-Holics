2339d497d6b23fb4a98ee646e909c471c174ae27
22AIML038RishiPatel <22aiml038@charusat.edu.in>
2023-04-02 13:40:31 +0530

    third commit

--- you_can_write_code_here.cpp
+++ you_can_write_code_here.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     cout << "Tech O'Holics!" << endl;
     cout << "2nd semester Group Project for CE144" << endl;
     cout << "Changes are made like this" << endl;
-    //22aiml021
-    cout<<"Vishal gandho che::";
+    // 22aiml021
+
     return 0;
 }
